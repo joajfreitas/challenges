7c4140dce8d2209998e503424da1c0ddbdc1d40d
joajfreitas <joaj.freitas@gmail.com>
2021-12-06 14:14:25 +0000

    day 6 advent of code 2021

---
+++ adventofcode/2021/6/2.py
@@ -0,0 +1,19 @@
+import sys
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+
+days = int(sys.argv[2])
+fishes = [int (x) for x in lines[0].split(",")]
+fishes = [fishes.count(i) for i in range(9)]
+
+for i in range(days):
+    print("day", i, sum(fishes))
+    n = fishes.pop(0)
+    fishes[6] += n
+    fishes.append(n)
+
+print(sum(fishes))
+
+
+
