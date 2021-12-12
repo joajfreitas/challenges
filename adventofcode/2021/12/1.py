0e7f9583495ab7490c3be033b926662b8cf66a25
joajfreitas <joaj.freitas@gmail.com>
2021-12-12 11:27:23 +0000

    day 12 advent of code 2021

---
+++ adventofcode/2021/12/1.py
@@ -0,0 +1,53 @@
+import sys
+from pprint import pprint
+
+
+def parse_edge(graph, line):
+    line = line.strip()
+    start, end = line.split("-")
+
+    if start not in graph.keys():
+        graph[start] = []
+
+    if end not in graph.keys():
+        graph[end] = []
+
+    if end not in graph[start]:
+        graph[start].append(end)
+
+    if start not in graph[end]:
+        graph[end].append(start)
+
+
+def search(graph):
+    def search_closure(graph, start, path, visited):
+        if start == "end":
+            return [path]
+
+        paths = []
+        ns = graph[start]
+        ns = [n for n in ns if n not in visited]
+        for n in ns:
+            if n == n.upper():
+                paths += search_closure(graph, n, path + [n], visited)
+            else:
+                paths += search_closure(graph, n, path + [n], visited + [n])
+
+        return paths
+
+    return search_closure(graph, "start", ["start"], ["start"])
+
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+
+graph = {}
+
+for line in lines:
+    parse_edge(graph, line)
+
+print(graph)
+
+paths = search(graph)
+pprint(paths)
+print(len(paths))
---
+++ adventofcode/2021/12/2.py
@@ -0,0 +1,60 @@
+import sys
+from pprint import pprint
+
+
+def parse_edge(graph, line):
+    line = line.strip()
+    start, end = line.split("-")
+
+    if start not in graph.keys():
+        graph[start] = []
+
+    if end not in graph.keys():
+        graph[end] = []
+
+    if end not in graph[start]:
+        graph[start].append(end)
+
+    if start not in graph[end]:
+        graph[end].append(start)
+
+
+def search(graph):
+    def search_closure(graph, start, path, visited):
+        if start == "end":
+            return [path]
+
+        paths = []
+        ns = graph[start]
+
+        def rule(n, ns, visited):
+            if len(visited) == len(set(visited)):
+                return visited.count(n) < 2 and n != "start"
+            else:
+                return n not in visited
+
+        ns = [n for n in ns if rule(n, ns, visited)]
+        for n in ns:
+            if n == n.upper():
+                paths += search_closure(graph, n, path + [n], visited)
+            else:
+                paths += search_closure(graph, n, path + [n], visited + [n])
+
+        return paths
+
+    return search_closure(graph, "start", ["start"], ["start"])
+
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+
+graph = {}
+
+for line in lines:
+    parse_edge(graph, line)
+
+print(graph)
+
+paths = search(graph)
+#pprint(paths)
+print(len(paths))
