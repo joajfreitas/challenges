354a0cd4c060b9b401681c18b6504cb112e6abd1
joajfreitas <joaj.freitas@gmail.com>
2022-04-24 23:05:19 +0100

    euler 1 and 2

---
+++ euler/1/main.py
@@ -0,0 +1,9 @@
+def multiple(x, n):
+    return x % n == 0
+
+multiple3 = lambda x : multiple(x, 3)
+multiple5 = lambda x : multiple(x, 5)
+multiple3or5 = lambda x: multiple3(x) or multiple5(x)
+
+
+print(sum(filter(multiple3or5, range(1000))))
---
+++ euler/2/main.py
@@ -0,0 +1,17 @@
+from functools import cache
+from itertools import takewhile
+
+@cache
+def fibonnaci(x):
+    if x == 0:
+        return 1
+    elif x == 1:
+        return 1
+    else:
+        return fibonnaci(x-1) + fibonnaci(x-2)
+
+def even(x):
+    return x % 2 == 0
+
+
+print(sum(filter(even, takewhile(lambda x: x<4e6, map(fibonnaci, range(int(4e6)))))))
