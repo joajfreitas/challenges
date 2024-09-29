de127667e79b74501bc52c62549c61646307b00d
João Freitas <joaj.freitas@gmail.com>
2024-09-29 22:06:58 +0100

    Implement -a flag for tee

--- tlpi/chapter04/4.1/tee.c
+++ tlpi/chapter04/4.1/tee.c
@@ -7,12 +7,25 @@
 #define BUF_SIZE 1024
 
 int main(int argc, char *argv[]) {
-    int fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    char buffer[BUF_SIZE] = {0};
+    
+    int flags = O_RDWR | O_CREAT;
+    char c = 0;
+    while((c = getopt(argc, argv, "a")) != -1) {
+        switch (c) {
+            case 'a':
+                flags |= O_APPEND;
+                break;
+            default:
+                printf("%c", c);
+                break;
+        }
+    }
+    
+    int fd = open(argv[optind], flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (fd == -1) {
         errExit("open");
     }
-    char buffer[BUF_SIZE] = {0};
-    
 
     while (true) {
         int nread = read(STDIN_FILENO, buffer, BUF_SIZE);
